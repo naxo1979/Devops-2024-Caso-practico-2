3806a7d04da2ab3c643095cea69232374d078925
naxo1979 <152732456+naxo1979@users.noreply.github.com>
2024-03-18 23:02:17 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,163 +1,215 @@
 
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
-# Definimos el proveedor y su version
 
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.95.0"
-    }
-  }
-}
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
+# Creación del recurso de red
 
-
-# Creamos el servicio principal
-
-provider "azurerm" {
-	# Configuration options
-	features {}
-	subscription_id = "cda335bf-ab3b-46d2-a3a0-df67c4f922c3"
-	client_id		= "26871334-6dde-4994-8980-0c2dddb877ec"
-	client_secret	= "tX~8Q~jrIzVtn7v73oQwGJmXYWUfr41RjF.9Aanb"
-	tenant_id		= "899789dc-202f-44b4-8472-a6d40f9eb440"
-	}
-
-# Creación del recurso en Azure
-
-resource "azurerm_resource_group" "rg" {
-  name     = "rg_cp2"
-  location = var.location
+resource "azurerm_virtual_network" "VMnetworkIPY" {
+  name                = "Virtual-network-IPY"
+  address_space       = ["172.16.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   
-	tags = {
+  tags = {
 	enviroment = "casopractico2"
 	}
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet
+# Creación de la subred
+
+resource "azurerm_subnet" "SubnetworkIPY" {
+  name                 = "SubnetworkIPYcp2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.VMnetworkIPY.name
+  address_prefixes     = ["172.16.0.0/24"]
   
 }
 
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
+# Creación del interface virtual - NIC - ip privada
 
-resource "azurerm_kubernetes_cluster" "k8s" {
+resource "azurerm_network_interface" "VMnicIPY" {
+  name                = "IPY-nic"
   location            = azurerm_resource_group.rg.location
-  name                = "clusteraks"
-  resource_group_name = "rg_cp2"
-  dns_prefix          = "clusteraksdns"
+  resource_group_name = azurerm_resource_group.rg.name
 
-  default_node_pool {
-    name       = "agenteaks"
-    vm_size    = "Standard_D2_v2"
-	node_count = 1
-  }
-
-  #node_resource_group = azurerm_resource_group.rg.name
-
-  identity {
-    type = "SystemAssigned"
-  }
+  ip_configuration {
+    name                          	= "ipprivvm"
+    subnet_id                     	= azurerm_subnet.SubnetworkIPY.id
+    private_ip_address_allocation 	= "Static"
+	private_ip_address				= "172.16.0.79"
+	public_ip_address_id			= azurerm_public_ip.PublicipIPY.id
+	}
 
   tags = {
-    Environment = "casopractico2"
-  }
+	enviroment = "casopractico2"
+	}
 }
 
+#Creación del interface virtual - ip pública
+#https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
 
-
-
-resource "azurerm_container_registry" "acr" {
-  name                = "UnirContainerIPY"
+resource "azurerm_public_ip" "PublicipIPY" {
+  name                = "PublicIp1"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Premium"
-  admin_enabled       = true
-  georeplications {
-    location                = "East US"
-    zone_redundancy_enabled = true
+  allocation_method   = "Dynamic"
+  sku				  = "Basic"
 
-  }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-
+  tags = {
+    environment = "casopractico2"
   }
 }
 
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/dns_a_record
 
-
-
-
-# Salida de ACR Login server
-
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+resource "azurerm_dns_zone" "dns-public-vm" {
+  name                = "ipypodmancp2.com"
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Salida de ACR username
-
-output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
-}
-
-# Salida de ACR password
-
-output "acr_admin_password" {
-  value = azurerm_container_registry.acr.admin_password
-  sensitive = true
-}
-
-# Generamos el fichero "auth.json"
-
-resource "local_file" "output_file" {
-  content = <<-EOF
-    ACR Login Server: ${azurerm_container_registry.acr.login_server}
-    ACR Admin Username: ${azurerm_container_registry.acr.admin_username}
-    ACR Admin Password: ${azurerm_container_registry.acr.admin_password}
-  EOF
-  filename = "/etc/containers/auth.json"
-}
-
-resource "local_file" "container_pass" {
-  content = <<-EOF
-${azurerm_container_registry.acr.admin_password}
-  EOF
-  filename = "/etc/containers/container_pass"
-}
-
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
-  sensitive = true
-}
-
-output "client_key" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
-  sensitive = true
-}
-
-output "cluster_ca_certificate" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
-  sensitive = true
-}
-
-output "cluster_password" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].password
-  sensitive = true
-}
-
-output "cluster_username" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
-  sensitive = true
+resource "azurerm_dns_a_record" "dnsvmrecord" {
+  name                = "dnsvm"
+  zone_name           = azurerm_dns_zone.dns-public-vm.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  target_resource_id  = azurerm_public_ip.PublicipIPY.id
 }
 
 
 
-resource "exportssh" "sshvm" {
+resource "azurerm_resource_group" "lbrg" {
+  name     = "LoadBalancerRG"
+  location = azurerm_resource_group.rg.location
+}
+
+# https://registry.terraform.io/providers/hashicorp/Azurerm/latest/docs/resources/public_ip
+
+resource "azurerm_public_ip" "publiciplb" {
+  name                = "PublicIPForLB"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/dns_a_record
+
+#resource "azurerm_dns_zone" "dns-public-aks" {
+#  name                = "ipynginxcp2.com"
+#  resource_group_name = azurerm_resource_group.rg.name
+#}
+#
+#resource "azurerm_dns_a_record" "dnsaksrecord" {
+#  name                = "dnsaks"
+#  zone_name           = azurerm_dns_zone.dns-public-aks.name
+#  resource_group_name = azurerm_resource_group.rg.name
+#  ttl                 = 300
+#  target_resource_id  = azurerm_public_ip.publiciplb.id
+#}
+
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/lb
+
+resource "azurerm_lb" "azurelb" {
+  name                = "AksLoadBalancer"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddressLB"
+    public_ip_address_id = azurerm_public_ip.publiciplb.id
+  }
+}
+
+
+
+
+
+resource "terraform_data" "azlogin" {
+
   provisioner "local-exec" {
-    command = "terraform output -raw ssh_private_key > /root/.ssh/vm_azure_devops"
+    command = "az login --service-principal -u 26871334-6dde-4994-8980-0c2dddb877ec -p tX~8Q~jrIzVtn7v73oQwGJmXYWUfr41RjF.9Aanb --tenant 899789dc-202f-44b4-8472-a6d40f9eb440"
+  }
+}
+
+# https://developer.hashicorp.com/terraform/language/meta-arguments/depends_on
+
+resource "terraform_data" "kubeconfig" {
+  depends_on =  [azurerm_kubernetes_cluster.k8s]
+  provisioner "local-exec" {
+    command = "az aks get-credentials --resource-group rg_cp2 --name clusteraks"
   }
 }
 
 
-resource "exportssh" "modsshvm" {
-  provisioner "local-exec" {
-    command = "chmod 600  /root/.ssh/vm_azure_devops"
+
+# Creamos el servicio de k8s
+
+#provider "kubernetes" {
+#  config_path = "~/.kube/config"
+#}
+#
+# https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/persistent_volume
+
+#resource "kubernetes_persistent_volume" "k8svolumenpersistente" {
+#  depends_on =  [azurerm_kubernetes_cluster.k8s]
+#  metadata {
+#    name = "k8svolumenpersistente"
+#  }
+#  spec {
+#    capacity = {
+#      storage = "1Gi"
+#    }
+#    access_modes = ["ReadWriteOnce"]
+#    persistent_volume_source {
+#      azure_disk {
+#        caching_mode  = "None"
+#        data_disk_uri = azurerm_managed_disk.azureidiscogestionado.id
+#        disk_name     = "discoaks"
+#        kind          = "Managed"
+#      }
+#    }
+#  }
+#}
+
+
+resource "azurerm_managed_disk" "azureidiscogestionado" {
+  name                 = "discoazuregestionado"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
+  tags = {
+    environment = azurerm_resource_group.rg.name
   }
+}
+
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  sensitive = true
+}
+
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.k8s.name
+}
+
+data "azurerm_public_ip" "PublicipIPY" {
+  name                = azurerm_public_ip.PublicipIPY.name
+  resource_group_name = azurerm_linux_virtual_machine.VMIPYDevops.resource_group_name
+}
+
+output "public_ip_address_vm" {
+  value = data.azurerm_public_ip.PublicipIPY.ip_address
+}
+
+data "azurerm_public_ip" "publiciplb" {
+  name                = azurerm_public_ip.publiciplb.name
+  resource_group_name = azurerm_lb.azurelb.resource_group_name
+}
+
+
+output "public_ip_address_lb" {
+  value = data.azurerm_public_ip.publiciplb.ip_address
 }

2635bd20ab88468ee4e386f421cf5ad81c76f8ed
naxo1979 <152732456+naxo1979@users.noreply.github.com>
2024-03-18 23:00:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,13 +35,13 @@ resource "azurerm_resource_group" "rg" {
   
 }
 
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster
+
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
   name                = "clusteraks"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = "rg_cp2"
   dns_prefix          = "clusteraksdns"
-
-
 
   default_node_pool {
     name       = "agenteaks"
@@ -49,6 +49,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 	node_count = 1
   }
 
+  #node_resource_group = azurerm_resource_group.rg.name
 
   identity {
     type = "SystemAssigned"
@@ -57,33 +58,8 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   tags = {
     Environment = "casopractico2"
   }
-
-
-#  ssh_key {
-#      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
-#    }
-  }
-
-
-resource "terraform_data" "azlogin" {
-
-  provisioner "local-exec" {
-    command = "az login --service-principal -u 26871334-6dde-4994-8980-0c2dddb877ec -p tX~8Q~jrIzVtn7v73oQwGJmXYWUfr41RjF.9Aanb --tenant 899789dc-202f-44b4-8472-a6d40f9eb440"
-  }
 }
 
-resource "terraform_data" "kubeconfig" {
-
-  provisioner "local-exec" {
-    command = "az aks get-credentials --resource-group rg_cp2 --name clusteraks"
-  }
-}
-
-# Cramos el servicio de k8s
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
 
 
 
@@ -104,41 +80,6 @@ resource "azurerm_container_registry" "acr" {
 
   }
 }
-
-resource "kubernetes_persistent_volume" "k8svolumenpersistente" {
-  metadata {
-    name = "k8svolumenpersistente"
-  }
-  spec {
-    capacity = {
-      storage = "1Gi"
-    }
-    access_modes = ["ReadWriteOnce"]
-    persistent_volume_source {
-      azure_disk {
-        caching_mode  = "None"
-        data_disk_uri = azurerm_managed_disk.azureidiscogestionado.id
-        disk_name     = "discoaks"
-        kind          = "Managed"
-      }
-    }
-  }
-}
-
-
-resource "azurerm_managed_disk" "azureidiscogestionado" {
-  name                 = "discoazuregestionado"
-  location             = azurerm_resource_group.rg.location
-  resource_group_name  = azurerm_resource_group.rg.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = "1"
-  tags = {
-    environment = azurerm_resource_group.rg.name
-  }
-}
-
-
 
 
 
@@ -174,13 +115,11 @@ resource "local_file" "output_file" {
   filename = "/etc/containers/auth.json"
 }
 
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
-  sensitive = true
-}
-
-output "kubernetes_cluster_name" {
-  value = azurerm_kubernetes_cluster.k8s.name
+resource "local_file" "container_pass" {
+  content = <<-EOF
+${azurerm_container_registry.acr.admin_password}
+  EOF
+  filename = "/etc/containers/container_pass"
 }
 
 output "client_certificate" {
@@ -206,4 +145,19 @@ output "cluster_password" {
 output "cluster_username" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
   sensitive = true
+}
+
+
+
+resource "exportssh" "sshvm" {
+  provisioner "local-exec" {
+    command = "terraform output -raw ssh_private_key > /root/.ssh/vm_azure_devops"
+  }
+}
+
+
+resource "exportssh" "modsshvm" {
+  provisioner "local-exec" {
+    command = "chmod 600  /root/.ssh/vm_azure_devops"
+  }
 }
